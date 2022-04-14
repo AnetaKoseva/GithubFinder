a533f87e25d5ff18db1a723c4cdd17e553ac1cf7
AnetaKoseva <62696878+AnetaKoseva@users.noreply.github.com>
2022-04-14 13:15:03 +0300

    before axios

--- src/context/github/GitHubContext.js
+++ src/context/github/GitHubContext.js
@@ -1,57 +1,28 @@
-import  {createContext,useReducer} from 'react'
-import gitHubReducer from './GitHubReducer'
+import { createContext, useReducer } from 'react'
+import GitHubReducer from './GitHubReducer'
 
-const GitHubContext=createContext()
+const GitHubContext = createContext()
 
-const GITHUB_URL=process.env.REACT_APP_GITHUB_URL
-const GITHUB_TOKEN=process.env.REACT_APP_GITHUB_TOKEN
+export const GitHubProvider = ({ children }) => {
+  const initialState = {
+    users: [],
+    user: {},
+    repos: [],
+    loading: false,
+  }
 
-export const GitHubProvider=({children})=>{
-    const initialState={
-        users:[],
-        loading:false
-    }
-    const [state, dispatch] =useReducer(gitHubReducer,initialState)
+  const [state, dispatch] = useReducer(GitHubReducer, initialState)
 
-//Get search results
-    const searchUsers=async(text)=>{
-        setLoading()
-
-        const params=new URLSearchParams({
-            q:text
-        })
-
-        const response=await fetch(`${GITHUB_URL}/search/users?${params}`,{
-            headers: {
-                Authorization: `token ${GITHUB_TOKEN}`
-            }
-        })
-
-        const {items}=await response.json()
-
-        dispatch({
-            type: 'GET_USERS',
-            payload:items,
-        })
-    }
-//Clear users
-    const clearUsers=()=>dispatch({
-        type: 'CLEAR_USERS'
-    })
-
-//Set loading
-    const setLoading=()=>dispatch({
-        type: 'SET_LOADING'
-    })
-
-    return <GitHubContext.Provider value={{
-        users: state.users,
-        loading: state.loading,
-        searchUsers,
-        clearUsers
-    }}>
-        {children}
+  return (
+    <GitHubContext.Provider
+      value={{
+        ...state,
+        dispatch,
+      }}
+    >
+      {children}
     </GitHubContext.Provider>
+  )
 }
 
 export default GitHubContext
